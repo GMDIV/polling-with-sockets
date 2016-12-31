15f98000afd2a8a1e860549693016b1a9e19d62f
Mitchell Deane <mitchelld20@yahoo.com>
2016-12-31 16:51:05 -0600

    linked the reactDOM render on index with the main App component successfully

---
+++ components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {};
+	}
+
+	render(){
+		return (
+			<div>
+				<h1>Welcome to the polling center!</h1>
+
+			</div>
+		)
+	}
+
+}

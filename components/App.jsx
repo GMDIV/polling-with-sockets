d90b248027dcf263913ae85920478ce91cd79e55
Mitchell Deane <mitchelld20@yahoo.com>
2016-12-31 18:59:15 -0600

    added basic socket.io functionality with confirmed ids

--- components/App.jsx
+++ components/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		
-		this.state = {};
+		this.state = {
+			data: {}
+		};
+	}
+
+	componentWillMount() {
+		this.socket = io('http://localhost:3000');
+		this.socket.on('connect', this.connect.bind(this));
+	}
+
+	connect(){
+		console.log("id", this.socket.id)
+		alert("Connected: " + this.socket.id)
 	}
 
 	render(){
 		return (
 			<div>
-				<h1>Welcome to the polling centerzzzzsdddddddddZ!</h1>
+				<h1>Welcome to the polling centerzzzzssdddddddddZ!</h1>
 
 			</div>
 		)
--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.use(express.static('./node_modules/bootstrap/dist'));
 //app.use(express.static(path.join(__dirname, '/../client/')))
 app.use (bodyParser.json());
 
-app.get('/app-bundle.js',
-	browserify('./components/index.jsx', {
-		transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
-	})
-)
+// app.get('/app-bundle.js',
+// 	browserify('./components/index.jsx', {
+// 		transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
+// 	})
+// )
 
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
 	console.log("The Polling server is running!! Port 3000");
 });
 
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+	console.log('Connected: %s', socket.id);
+})

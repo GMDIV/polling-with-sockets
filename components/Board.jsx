3dbf25b8ce6710c83073543dc03a18b7c3f0b118
Mitchell Deane <mitchelld20@yahoo.com>
2017-01-02 23:56:17 -0600

    Added Board, Audience, and Speaker component shells.

---
+++ components/Board.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Board extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h1>
+					Board
+				</h1>
+			</div>
+
+		)
+	}
+}
---
+++ components/Speaker.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Speaker extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h1>
+					Speaker
+				</h1>
+			</div>
+
+		)
+	}
+}
